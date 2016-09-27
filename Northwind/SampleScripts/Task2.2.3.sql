eeb4dce4bf80a32761da4317a97fe0820f2c89bd
Alsu Islamova <Alsu_Islamova@epam.com>
2016-09-27 21:32:42 +0600

    Add 2 tasks

---
+++ Northwind/SampleScripts/Task2.2.3.sql
@@ -0,0 +1,7 @@
+﻿Select o.EmployeeID, o.CustomerID, count(o.CustomerID) as ordersCount
+from Orders as o
+where Year(o.OrderDate) ='1998'
+group by o.EmployeeID, o.CustomerID
+/*order by o.EmployeeID -- sorting for visible results*/
+
+
